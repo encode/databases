c18b19c8788d304f17ee10a78691af756b19dcd9
Tom Christie <tom@tomchristie.com>
2020-04-23 14:45:53 +0100

    Rollback #158

--- databases/core.py
+++ databases/core.py
@@ -75,6 +75,12 @@ class Database:
         self._global_connection = None  # type: typing.Optional[Connection]
         self._global_transaction = None  # type: typing.Optional[Transaction]
 
+        if self._force_rollback:
+            self._global_connection = Connection(self._backend)
+            self._global_transaction = self._global_connection.transaction(
+                force_rollback=True
+            )
+
     async def connect(self) -> None:
         """
         Establish the connection pool.
@@ -88,13 +94,7 @@ class Database:
         self.is_connected = True
 
         if self._force_rollback:
-            assert self._global_connection is None
-            assert self._global_transaction is None
-
-            self._global_connection = Connection(self._backend)
-            self._global_transaction = self._global_connection.transaction(
-                force_rollback=True
-            )
+            assert self._global_transaction is not None
 
             await self._global_transaction.__aenter__()
 
@@ -105,13 +105,9 @@ class Database:
         assert self.is_connected, "Already disconnected."
 
         if self._force_rollback:
-            assert self._global_connection is not None
             assert self._global_transaction is not None
 
             await self._global_transaction.__aexit__()
-
-            self._global_transaction = None
-            self._global_connection = None
 
         await self._backend.disconnect()
         logger.info(
@@ -189,12 +185,15 @@ class Database:
 
     @contextlib.contextmanager
     def force_rollback(self) -> typing.Iterator[None]:
-        initial = self._force_rollback
+        self._global_connection = Connection(self._backend)
+        self._global_transaction = self._global_connection.transaction(
+            force_rollback=True
+        )
         self._force_rollback = True
         try:
             yield
         finally:
-            self._force_rollback = initial
+            self._force_rollback = False
 
 
 class Connection:
