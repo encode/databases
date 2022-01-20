10efc31c4513d41b9fd003bf4b754e704ef7911f
collerek <collerek@gmail.com>
2022-01-20 18:48:16 +0100

    fix black (reminder to myself - don't use github web ide :F)

--- tests/test_databases.py
+++ tests/test_databases.py
@@ -1219,7 +1219,7 @@ async def test_mapping_property_interface(database_url):
         query = notes.insert()
         values = {"text": "example1", "completed": True}
         await database.execute(query, values)
-        
+
         query = notes.select()
         single_result = await database.fetch_one(query=query)
         assert single_result._mapping["text"] == "example1"
@@ -1229,7 +1229,7 @@ async def test_mapping_property_interface(database_url):
         assert list_result[0]._mapping["text"] == "example1"
         assert list_result[0]._mapping["completed"] is True
 
-        
+
 @pytest.mark.parametrize("database_url", DATABASE_URLS)
 @mysql_versions
 @async_adapter
@@ -1240,11 +1240,10 @@ async def test_result_named_access(database_url):
     async with Database(database_url) as database:
         query = notes.insert()
         values = {"text": "example1", "completed": True}
-        await database.execute(query, values)      
-        
+        await database.execute(query, values)
+
         query = notes.select().where(notes.c.text == "example1")
         result = await database.fetch_one(query=query)
 
         assert result.text == "example1"
         assert result.completed is True
-
