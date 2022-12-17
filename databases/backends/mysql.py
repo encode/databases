81cc6fdb1ce4e78875960a8a262a4b134745946e
jonium <52005121+joniumGit@users.noreply.github.com>
2022-12-17 13:27:06 +0200

    Fixes breaking changes in SQLAlchemy cursor (#513)
    Co-authored-by: Marcelo Trylesinski <marcelotryle@gmail.com>
    fixes undefined

--- databases/backends/mysql.py
+++ databases/backends/mysql.py
@@ -211,6 +211,7 @@ class MySQLConnection(ConnectionBackend):
                 compiled._result_columns,
                 compiled._ordered_columns,
                 compiled._textual_ordered_columns,
+                compiled._ad_hoc_textual,
                 compiled._loose_column_name_matching,
             )
         else:
--- databases/backends/sqlite.py
+++ databases/backends/sqlite.py
@@ -185,6 +185,7 @@ class SQLiteConnection(ConnectionBackend):
                 compiled._result_columns,
                 compiled._ordered_columns,
                 compiled._textual_ordered_columns,
+                compiled._ad_hoc_textual,
                 compiled._loose_column_name_matching,
             )
 
